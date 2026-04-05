0b2c018b2841fdd48ef7f2d8f6109c19ebe32f98
Marius Cerwenetz <49414340+mcerwenetz@users.noreply.github.com>
2026-04-05 16:23:41 +0200

    first sketch pseudocode

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import datetime
 from waitress import serve
 
 app = Flask(__name__)
 
+records = []
 
 # Homepage - Liste aller Notizen
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('base.html', notes=notes)
+    if request.methods == 'POST':
+        records.append(request.args.to_dict())
+    else:
 
+        timestamps = [ entry['timestamp'] for enty in records]
 
-
+        if len(records <= 5):
+            return render_template("no_records.html")
+        
+        elif datetime.datetime.now() - max(timestamps) < 1000:
+            records = []
+            return render_template("no_records.html")
+        else:
+            return render_template('base.html', notes=notes)
 
 
 if __name__ == '__main__':

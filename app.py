9fe1e92bd89504633b191bdea8d5537f3550a350
Marius <marius.cerwenetz@mailbox.org>
2026-04-07 21:22:41 +0200

    example working with waitress

--- app.py
+++ app.py
@@ -1,32 +1,42 @@
 from flask import Flask, render_template, request
-import datetime
 import json
-#from waitress import serve
+import time
+from waitress import serve
 
 app = Flask(__name__)
 
 
 records = []
 # Homepage - Liste aller Notizen
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
     global records
+    now = int(time.time() * 1000 )
 
-    if request.method == 'POST':
+    if request.args:
         entry = request.args.to_dict()
         records.append(entry)
+        # dont store more than 100 records
+        if len(records) > 99:
+            records.remove(records[0])
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
-    elif request.method == 'GET':
 
-        if len(records) <= 0:
-            return render_template("no_records.html")
-        
-        #elif str(datetime.datetime.now()) - records[-1]['timestamp'] < 1000:
-        #    records = []
-        #    return render_template("no_records.html")
-        else:
-            return render_template('base.html', loc=records[-1])
+
+    # show only for consecutive records, not just the first one
+    if len(records) <= 5:
+        return render_template("no_records.html")
+    else:
+        latest = int(records[-1]['timestamp'])
+        diff = now - latest
+    
+    
+    # delete all records if the latest record is older than 1 min
+    if diff > 60000:
+        records = []
+        return render_template("no_records.html")
+    else:
+        return render_template('base.html', loc=records[-1])
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080)
+    serve(app, host='0.0.0.0', port=8080, url_scheme='https')
